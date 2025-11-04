757e9f2838b2edfddcbadca821773c13f93c8910
Asmit Shukla <asmitshukla9794@gmail.com>
2025-11-05 00:30:50 +0530

    new changes made

--- webdev/week3.3/index.js
+++ webdev/week3.3/index.js
@@ -1,9 +1,21 @@
 
 function addtask(){
+    console.log("clicked");
     const div=document.createElement("div");
+    div.className="boxdiv";
     const h2=document.createElement("h2");
+    h2.className="boxh2";
     const h4=document.createElement("h4");
-    div.innerHTML=document
+    h4.className="boxh4";
+    h2.innerHTML=document.querySelector(".a").value;
+    h4.innerHTML=document.querySelector(".b").value;
+
+    div.appendChild(h2);
+    div.appendChild(h4);
+
+    const main=document.querySelector(".to-do");
+    main.appendChild(div);
+
 
 
     const inadd=document.getElementsByClassName("btn");
